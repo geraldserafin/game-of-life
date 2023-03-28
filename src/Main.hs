32a3671be45f74a8d2d51edb736dab6eda4fbd09
geraldserafin <serafingerald@protonmail.com>
2023-03-28 12:07:18 +0200

    Parametrize quadtree to fit game of life needs

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,17 @@
 module Main where
 
-import Quadtree (leaf, insert, query, points, pointsInDistance)
+import Graphics.Gloss (Point)
+import Quadtree (Quadtree (Empty), getPointsNeighbours, insert)
+
+data Cell = Alive | Dead deriving (Eq, Show)
+
+aliveNeighbours :: Quadtree Cell -> [(Point, Cell, Int)]
+aliveNeighbours = map (\(a,b,c) -> (a,b, length $ filter (==Alive) c)) . getPointsNeighbours
+
+insertAlive :: (Float, Float) -> Quadtree Cell -> Quadtree Cell
+insertAlive (x,y) t = foldl (\t' p -> insert p Dead t') (insert (x,y) Alive t) neighbourIndexes 
+  where 
+    neighbourIndexes = [(x',y') | x' <- [x-1..x+1], y' <- [y-1..y+1], (x',y') /= (x,y) ]
 
 main :: IO ()
-main = print . pointsInDistance 1 . insert (-1,-1) . insert (1,1) . insert (2,2) $ insert (0,0) leaf
+main = print . aliveNeighbours . insertAlive (0,0) $ Empty
--- src/Quadtree.hs
+++ src/Quadtree.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE TupleSections #-}
 module Quadtree where
 
 import Graphics.Gloss (Point)
-import qualified Graphics.Gloss.Data.Point.Arithmetic as P ((+), (-)) 
+import qualified Graphics.Gloss.Data.Point.Arithmetic as P ((+), (-))
 import Data.List (nub)
 
 --     |  
@@ -11,10 +12,11 @@ import Data.List (nub)
 --     |
 data IntercardinalDirection = NE | SE | SW | NW deriving (Show, Eq)
 
--- Point quadtree date structure
-data Quadtree = Node { point :: Point, ne, se, sw, nw :: Quadtree }
-              | Leaf (Maybe Point)
-              deriving (Show)
+-- Point quadtree data structure
+data Quadtree a = Node { point :: (Point, a), ne, se, sw, nw :: Quadtree a }
+                | Leaf Point a
+                | Empty
+                deriving (Show)
 
 -- intercardinal direction of (x2,y2) point relative to (x1,y1) point
 intercardinalDirection :: Point -> Point -> IntercardinalDirection
@@ -24,28 +26,24 @@ intercardinalDirection (x1,y1) (x2,y2)
   | x1 >= x2 && y1 <  y2 = NW
   | otherwise = NE
 
--- Empty quadtree
-leaf :: Quadtree
-leaf = Leaf Nothing
-
 -- Node with point
-node :: Point -> Quadtree
-node x = Node x leaf leaf leaf leaf
+node :: (Point, a) -> Quadtree a
+node x = Node x Empty Empty Empty Empty
 
 -- Inserting element into quadtree
-insert :: Point -> Quadtree -> Quadtree
-insert p (Leaf Nothing)  = Leaf (Just p)
-insert p (Leaf (Just v)) = insert p $ node v
-insert p n@(Node v ne' se' sw' nw')
-  | dir == NE = n { ne = insert p ne' }
-  | dir == SE = n { se = insert p se' }
-  | dir == SW = n { sw = insert p sw' }
-  | otherwise = n { nw = insert p nw' }
+insert :: Point -> a -> Quadtree a -> Quadtree a
+insert p a Empty  = Leaf p a
+insert p1 v1 l@(Leaf p2 v2) = if p1 /= p2 then insert p1 v1 $ node (p2,v2) else l
+insert p1 v1 n@(Node (p2,_) ne' se' sw' nw')
+  | dir == NE = n { ne = insert p1 v1 ne' }
+  | dir == SE = n { se = insert p1 v1 se' }
+  | dir == SW = n { sw = insert p1 v1 sw' }
+  | otherwise = n { nw = insert p1 v1 nw' }
   where
-    dir = intercardinalDirection v p
+    dir = intercardinalDirection p2 p1
 
 -- Mapping intercardinal directions to quadtree quarters
-quarter :: IntercardinalDirection -> Quadtree -> Quadtree
+quarter :: IntercardinalDirection -> Quadtree a -> Quadtree a
 quarter NE = ne
 quarter SE = se
 quarter SW = sw
@@ -56,27 +54,33 @@ quarter NW = nw
 --  |  P3  |
 --  |      |
 -- P1 ---- |
-pointInBox :: Point -> Point -> Point -> Bool
-pointInBox (x1,y1) (x2,y2) (x3,y3) 
+contains :: (Point, Point) -> Point  -> Bool
+contains ((x1,y1), (x2,y2)) (x3,y3)
   = x1 <= x3 && x3 <= x2 &&
     y1 <= y3 && y3 <= y2
 
--- Query all points covered by box
-query :: Point -> Point -> Quadtree -> [Point]
-query _  _  (Leaf Nothing)  = []
-query p1 p2 (Leaf (Just v)) = [v | pointInBox p1 p2 v]
-query p1@(x1, y1) p2@(x2, y2) n = subPoints ++ [v | pointInBox p1 p2 v]
+-- Get all points covered by a rectangular box
+query :: Point -> Point -> Quadtree a -> [(Point, a)]
+query _  _  Empty  = []
+query p1 p2 (Leaf c v) = [(c,v) | (p1,p2) `contains` c]
+query p1@(x1, y1) p2@(x2, y2) n = subPoints ++ [(c,v) | (p1,p2) `contains` c]
   where
-    (p3, p4, v) = ((x2, y1), (x1, y2), point n)
-    subPoints   = concatMap ((\dir -> query p1 p2 $ dir n) . quarter) directions
-    directions  = nub $ map (intercardinalDirection v) [p1,p2,p3,p4]
+    (p3, p4, (c,v)) = ((x2, y1), (x1, y2), point n)
+    subPoints  = concatMap ((\q -> query p1 p2 $ q n) . quarter) directions
+    directions = nub $ map (intercardinalDirection c) [p1,p2,p3,p4]
 
--- get all points in tree
-points :: Quadtree -> [Point]
-points (Leaf Nothing)  = []
-points (Leaf (Just v)) = [v]
-points (Node v ne' se' sw' nw') = v : points ne' ++ points se' ++ points sw' ++ points nw' 
+-- Get all points in tree
+points :: Quadtree a -> [(Point, a)]
+points Empty = []
+points (Leaf v a) = [(v,a)]
+points (Node v ne' se' sw' nw') = v : points ne' ++ points se' ++ points sw' ++ points nw'
 
--- get all points within distance r for every point in tree
-pointsInDistance :: Float -> Quadtree -> [(Point, [Point])]
-pointsInDistance r q = map (\x -> (,) x . filter (/=x) $ query (x P.- (r,r)) (x P.+ (r,r)) q) $ points q
+-- Get all values of points within distance 1 for given point
+getPointNeighbours :: (Point, a) -> Quadtree a -> (Point, a, [a])
+getPointNeighbours (p1,v) = (p1,v,) 
+  . map snd . filter ((/=p1) . fst) 
+  . query (p1 P.- (1,1)) (p1 P.+ (1,1)) 
+
+-- Get all points within distance 1 for every point in tree
+getPointsNeighbours :: Quadtree a -> [(Point, a, [a])]
+getPointsNeighbours q = map (`getPointNeighbours` q) $ points q
